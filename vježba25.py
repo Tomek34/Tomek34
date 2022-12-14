4f133ed32950cf704d5d31aa65272db29612be44
Tomek34 <120574057+Tomek34@users.noreply.github.com>
2022-12-14 19:33:06 +0100

    Add files via upload

--- vježba25.py
+++ vježba25.py
@@ -6,12 +6,82 @@
     # kW (kilowatt) u ks (horsepower ili konjska snaga) - 1 kW = 1.3596ks
 
 
+"""konverzija_km_u_milju = 0.06214
 
-konverzija = int(input("Unesite broj koji želite konvertirati: "))
-duljina_u_kilometrima = konverzija*0.6214
-duljina_u_miljama = konverzija*1.609344
 
-if konverzija == duljina_u_kilometrima:
-    print(duljina_u_miljama*1.609344)
-if konverzija == duljina_u_miljama:
-    print(duljina_u_kilometrima*0.6214)
+while True:
+    # ALGORITAM
+    jedinica = input("Odaberi jedinicu 'km' za kilometre u milje , 'm' za milje u kilometre: ")
+    if jedinica == "km":
+        duljina = int(input("Unesi broj kilometara: "))
+        print(f'{duljina} kilometara je {duljina*konverzija_km_u_milju:.4f} milja.')
+    elif jedinica == "m":
+        duljina = int(input("Unesi broj milja: "))
+        print(f'{duljina} milja je {duljina/konverzija_km_u_milju:.4f} kilometara.')
+    else:
+        print("Ne znam pretvoriti ovo.")
+        continue
+
+    # DIO ZA PROVJERU 
+    odgovor = int(input('Želite li prekinuti izvršavanje programa? (Da=1/Ne=0) '))
+    if odgovor == 1:
+        print('\nViše nema potencijalno beskonačne WHILE petlje s True uvjetom.\n Upravo je završila :-) \n\n')
+        break
+    else:
+        continue
+print()"""
+
+
+"""konverzija_c_u_f = 32
+
+while True:
+    jedinica = input("Odaberi jedinicu 'C' za celzijuse u farenheite, 'F' za farenheite u celzijuse: ")
+    if jedinica == "C":
+        temperatura = int(input("Unesi broj celzijusa: "))
+        print(f'{temperatura} u celzijusima je {temperatura*konverzija_c_u_f:.4f} stupnjeva celzijusevih.')"""
+
+
+"""konverzija_kg_u_funtu = 2.2046
+
+while True:
+    jedinica = input("Odaberi jedinicu 'kg' za kilograme u funte, 'p' za funte u kilograme: ")
+    if jedinica == "kg":
+        kilaža = int(input("Unesi broj kilograma: "))
+        print(f'{kilaža} u kilogramima je {kilaža*konverzija_kg_u_funtu:.4f} kilograma.')
+    elif jedinica == "p":
+        kilaža = int(input("Unesi broj funti: "))
+        print(f'{kilaža} u funtama je {kilaža/konverzija_kg_u_funtu:.4f} funti.')
+    else:
+        print("Ne znam pretvoriti ovo.")
+        continue
+
+    odgovor = int(input('Želite li prekinuti izvršavanje programa? (Da=1/Ne=0 '))
+    if odgovor == 1:
+        print('\nViše nema potencijalno beskonačne WHILE petlje s True uvjetom.\n Upravo je završila :-) \n\n')
+        break
+    else:
+        continue
+print()"""
+
+
+konverzija_litara_u_galon = 0.2642
+
+while True:
+    jedinica = input("Odaberi jedinicu 'l' za litre u galone, 'gal' za galone u litre: ")
+    if jedinica == "l":
+        volumen = int(input("Unesi broj litara: "))
+        print(f'{volumen} u litrama je {volumen*konverzija_litara_u_galon:.4} litara.')
+    elif jedinica == "gal":
+        volumen = int(input("Unesi broj galona: "))
+        print(f'{volumen} u galonima je {volumen/konverzija_litara_u_galon:.4} galona.')
+    else:
+        print("Ne znam pretvoriti ovo.")
+        continue
+
+    odgovor = int(input('Želite li prekinuti izvršavanje programa? (Da=1/Ne=0 '))
+    if odgovor == 1:
+        print('\nViše nema potencijalno beskonačne WHILE petlje s True uvjetom.\n Upravo je završila :-) \n\n')
+        break
+    else:
+        continue
+print()
