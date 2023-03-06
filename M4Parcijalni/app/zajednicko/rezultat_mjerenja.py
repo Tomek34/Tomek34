3e62e2ecd16506d103537cf058465437443fcd3d
Tomek34 <120574057+Tomek34@users.noreply.github.com>
2023-03-06 19:30:12 +0100

    Add files via upload

--- M4Parcijalni/app/zajednicko/rezultat_mjerenja.py
+++ M4Parcijalni/app/zajednicko/rezultat_mjerenja.py
@@ -4,8 +4,23 @@ class RezultatMjerenja:
         self.mjerna_jedinica = mjerna_jedinica
         self.tip = tip
 
+def lijepi_ispis(self):
+    print(f"Vrijednost {self.tip} mjerenja je {self.vrijednost}{self.mjerna_jedinica}")    
+    
+    # ZADATAK:
+    # napraviti metou u ovoj klasi koja prima objekt koje je instanca ove klase i
+    # vraća True ako je:
+    # - objekt istog tipa kao i objekt koji smo primili kao argument klase
+    # - vrijednost oba objekta ista
+    # u svim ostalim slučajevima vraća False
+
+    # odnosno radi kao i ovo na linijama 22 i 23
+
+
 rez_1 = RezultatMjerenja(100, '°C', "TEMPERATURA")
 rez_2 = RezultatMjerenja(90, '°C', "TEMPERATURA")
+rez_3 = RezultatMjerenja(90, '°C', "TEMPERATURA")
+rez_4 = RezultatMjerenja(90, 'hPa', "TLAK")
 
 if rez_1.tip == rez_2.tip:
     print(rez_1.vrijednost == rez_2.vrijednost)
